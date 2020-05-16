680a0cd04b67cc14cd44e113f82a8dd7d214283f
6 <ernest1800@protonmail.com>
2020-05-16 02:02:42 +0100

    tests with aspx

--- FootballMerchTests/tstOrder.cs
+++ FootballMerchTests/tstOrder.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Security.Policy;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace FootballMerchTests
@@ -7,10 +8,54 @@ namespace FootballMerchTests
     public class tstOrder
     {
         [TestMethod]
-        public void TestMethod1()
+        public void InstanceOK()
         {
             clsOrder order = new clsOrder();
             Assert.IsNotNull(order);
+        }
+        [TestMethod]
+        public void OrderIDOK()
+        {
+            clsOrder order = new clsOrder();
+            Int32 TestData = 1;
+            order.OrderID = TestData;
+            Assert.AreEqual(order.OrderID, TestData);
+        }
+
+        [TestMethod]
+        public void CustomerIDOK()
+        {
+            clsOrder order = new clsOrder();
+            Int32 TestData = 1;
+            order.CustomerID = TestData;
+            Assert.AreEqual(order.CustomerID, TestData);
+        }
+
+        [TestMethod]
+        public void ShippingAddressOK()
+        {
+            clsOrder order = new clsOrder();
+            String TestData = "Red Road";
+            order.ShippingAddress = TestData;
+            Assert.AreEqual(order.ShippingAddress, TestData);
+        }
+
+
+        [TestMethod]
+        public void OrderDateOK()
+        {
+            clsOrder order = new clsOrder();
+            DateTime TestData = DateTime.Now.Date;
+            order.OrderDate = TestData;
+            Assert.AreEqual(order.OrderDate, TestData);
+        }
+
+        [TestMethod]
+        public void OrderShippedOK() {
+            clsOrder order = new clsOrder();
+            Boolean TestData = true;
+            order.OrderShipped= TestData;
+            Assert.AreEqual(order.OrderShipped, TestData);
         }
     }
 }
