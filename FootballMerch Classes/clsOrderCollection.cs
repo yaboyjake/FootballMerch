01535440796b0e7fded8a1d551844c942ca17d50
6 <ernest1800@protonmail.com>
2020-05-17 06:17:07 +0100

    All order component completed

--- FootballMerch Classes/clsOrderCollection.cs
+++ FootballMerch Classes/clsOrderCollection.cs
@@ -6,28 +6,14 @@ namespace FootballMerch_Classes
     public class clsOrderCollection
     {
         private List<clsOrder> mOrderList = new List<clsOrder>();
-
+        private clsOrder mThisOrder = new clsOrder();
         public clsOrderCollection()
         {
             Int32 Index = 0;
             Int32 RecordCount = 0;
             clsDataConnection DB = new clsDataConnection();
             DB.Execute("sproc_tblOrder_SelectAll");
-            RecordCount = DB.Count;
-
-            while (Index < RecordCount)
-            {
-                clsOrder Order = new clsOrder();
-
-                Order.OrderID = Convert.ToInt32(DB.DataTable.Rows[Index]["OrderID"]);
-                Order.CustomerID = Convert.ToInt32(DB.DataTable.Rows[Index]["CustomerID"]);
-                Order.ShippingAddress = Convert.ToString(DB.DataTable.Rows[Index]["ShippingAddress"]);
-                Order.OrderDate = Convert.ToDateTime(DB.DataTable.Rows[Index]["OrderDate"]);
-                Order.OrderShipped = Convert.ToBoolean(DB.DataTable.Rows[Index]["OrderShipped"]);
-
-                mOrderList.Add(Order);
-                Index++;
-            }
+            PopulateArray(DB);
 
         }
         
@@ -54,6 +40,73 @@ namespace FootballMerch_Classes
 
             }
         }
-        public clsOrder ThisOrder { get; set; }
+        public clsOrder ThisOrder
+        {
+            get
+            {
+                return mThisOrder;
+            }
+            set
+            {
+                mThisOrder = value;
+            }
+        }
+
+        public int Add()
+        {
+            clsDataConnection DB = new clsDataConnection();
+            DB.AddParameter("@CustomerID", mThisOrder.CustomerID);
+            DB.AddParameter("@ShippingAddress", mThisOrder.ShippingAddress);
+            DB.AddParameter("@OrderDate", mThisOrder.OrderDate);
+            DB.AddParameter("@OrderShipped", mThisOrder.OrderShipped);
+            return DB.Execute("sproc_tblOrder_Insert");
+        }
+
+        public void Delete()
+        {
+            clsDataConnection DB = new clsDataConnection();
+            DB.AddParameter("@OrderID", mThisOrder.OrderID);
+            DB.Execute("sproc_tblOrder_Delete");
+        }
+
+        public void Update()
+        {
+            clsDataConnection DB = new clsDataConnection();
+            DB.AddParameter("@OrderID", mThisOrder.OrderID);
+            DB.AddParameter("@CustomerID", mThisOrder.CustomerID);
+            DB.AddParameter("@ShippingAddress", mThisOrder.ShippingAddress);
+            DB.AddParameter("@OrderDate", mThisOrder.OrderDate);
+            DB.AddParameter("@OrderShipped", mThisOrder.OrderShipped);
+            DB.Execute("sproc_tblOrder_Update");
+        }
+
+        public void ReportByShippingAddress(string ShippingAddress)
+        {
+            clsDataConnection DB = new clsDataConnection();
+            DB.AddParameter("@ShippingAddress", ShippingAddress);
+            DB.Execute("sproc_tblOrder_FilterByShippingAddress");
+            PopulateArray(DB);
+        }
+
+        void PopulateArray(clsDataConnection DB)
+        {
+            Int32 Index = 0;
+            Int32 RecordCount;
+            RecordCount = DB.Count;
+            mOrderList = new List<clsOrder>();
+            while (Index < RecordCount)
+            {
+                clsOrder Order = new clsOrder();
+
+                Order.OrderID = Convert.ToInt32(DB.DataTable.Rows[Index]["OrderID"]);
+                Order.CustomerID = Convert.ToInt32(DB.DataTable.Rows[Index]["CustomerID"]);
+                Order.ShippingAddress = Convert.ToString(DB.DataTable.Rows[Index]["ShippingAddress"]);
+                Order.OrderDate = Convert.ToDateTime(DB.DataTable.Rows[Index]["OrderDate"]);
+                Order.OrderShipped = Convert.ToBoolean(DB.DataTable.Rows[Index]["OrderShipped"]);
+
+                mOrderList.Add(Order);
+                Index++;
+            }
+        }
     }
 }
--- FootballMerch FrontEnd/OrderList.aspx.cs
+++ FootballMerch FrontEnd/OrderList.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using FootballMerch_Classes;
 
 public partial class OrderList : System.Web.UI.Page
 {
@@ -17,15 +18,78 @@ public partial class OrderList : System.Web.UI.Page
 
     void DisplayOrders()
     {
-        FootballMerch_Classes.clsOrderCollection Orders = new FootballMerch_Classes.clsOrderCollection();
+        clsOrderCollection Orders = new clsOrderCollection();
         lstOrderList.DataSource = Orders.OrderList;
         lstOrderList.DataValueField = "OrderID";
-        lstOrderList.DataTextField = "CustomerID";
+        //lstOrderList.DataTextField = "CustomerID";
+        lstOrderList.DataTextField = "ShippingAddress";
         lstOrderList.DataBind();
 
     }
     protected void lstOrderList_SelectedIndexChanged(object sender, EventArgs e)
     {
 
+    }
+
+    protected void btnAdd_Click(object sender, EventArgs e)
+    {
+        Session["OrderID"] = -1;
+        Response.Redirect("order.aspx");
+    }
+
+    protected void btnDelete_Click(object sender, EventArgs e)
+    {
+        Int32 OrderID;
+
+        if (lstOrderList.SelectedIndex != -1)
+        {
+            OrderID = Convert.ToInt32(lstOrderList.SelectedValue);
+            Session["OrderID"] = OrderID;
+            Response.Redirect("deleteOrder.aspx");
+
+        }
+        else
+        {
+            //no record selected
+            lblError.Text = "Please select a record to delete";
+        }
+    }
+
+    protected void btnEdit_Click(object sender, EventArgs e)
+    {
+        Int32 OrderID;
+
+        if (lstOrderList.SelectedIndex != -1)
+        {
+            OrderID = Convert.ToInt32(lstOrderList.SelectedValue);
+            Session["OrderID"] = OrderID;
+            Response.Redirect("order.aspx");
+
+        }
+        else
+        {
+            //no record selected
+            lblError.Text = "Please select a record to edit";
+        }
+    }
+
+    protected void btnApply_Click(object sender, EventArgs e)
+    {
+        clsOrderCollection Orders = new clsOrderCollection();
+        Orders.ReportByShippingAddress(txtFilter.Text);
+        lstOrderList.DataSource = Orders.OrderList;
+        lstOrderList.DataValueField = "OrderID";
+        lstOrderList.DataTextField = "ShippingAddress";
+        lstOrderList.DataBind();
+    }
+
+    protected void btnClear_Click(object sender, EventArgs e)
+    {
+        clsOrderCollection Orders = new clsOrderCollection();
+        Orders.ReportByShippingAddress("");
+        lstOrderList.DataSource = Orders.OrderList;
+        lstOrderList.DataValueField = "OrderID";
+        lstOrderList.DataTextField = "ShippingAddress";
+        lstOrderList.DataBind();
     }
 }
