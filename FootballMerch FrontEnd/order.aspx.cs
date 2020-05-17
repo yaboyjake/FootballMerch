88bec1e8a8c45a0dd3f498cfd8a69c440af1e204
6 <ernest1800@protonmail.com>
2020-05-17 02:49:53 +0100

    Progress

--- FootballMerch FrontEnd/order.aspx.cs
+++ FootballMerch FrontEnd/order.aspx.cs
@@ -10,22 +10,55 @@ public partial class order : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        clsOrder Order = new clsOrder();
-        Order = (clsOrder)Session["Order"];
-        Response.Write(Order.OrderID);
+       // clsOrder Order = new clsOrder();
+        //Order = (clsOrder)Session["Order"];
+        //Response.Write(Order.OrderID);
 
     }
 
-    protected void btnOK_Click(object sender, EventArgs e)
+    protected void btnOK_Click1(object sender, EventArgs e)
     {
         clsOrder Order = new clsOrder();
-        Order.OrderID = Int32.Parse(txtOrderID.Text);
-        Order.CustomerID = Int32.Parse(txtCustomerID.Text);
-        Order.ShippingAddress= txtShippingAddress.Text;
-        Order.OrderShipped = chkShipped.Checked;
-        Order.OrderDate = Convert.ToDateTime(txtOrderDate.Text);
-        Session["Order"] = Order;
-        Response.Redirect("OrderViewer.aspx");
+
+        string CustomerID = txtCustomerID.Text;
+        string ShippingAddress = txtShippingAddress.Text;
+        string OrderDate = txtOrderDate.Text;
+
+        string Error = "";
+
+        Error = Order.Valid(CustomerID, ShippingAddress, OrderDate);
+        if (Error == "")
+        {
+            Order.CustomerID = Convert.ToInt32(CustomerID);
+            Order.ShippingAddress = ShippingAddress;
+            Order.OrderDate = Convert.ToDateTime(OrderDate);
+            Session["Order"] = Order;
+            lblError.Text = "OrderSubmitted";
+            Response.Write("orderViewer.aspx");
+        }
+        else
+        {
+            lblError.Text = Error;
+        }
         
     }
+
+    protected void btnFind_Click(object sender, EventArgs e)
+    {
+        clsOrder Order = new clsOrder();
+        Int32 OrderID;
+        Boolean Found = true;
+        OrderID = Convert.ToInt32(txtOrderID.Text);
+        Found = Order.Find(OrderID);
+        if (Found == true)
+        {
+            txtCustomerID.Text = Convert.ToString(Order.CustomerID);
+            txtShippingAddress.Text = Convert.ToString(Order.ShippingAddress);
+            txtOrderDate.Text = Convert.ToString(Order.OrderDate);
+            chkShipped.Checked = Order.OrderShipped;
+            
+        }
+    }
+
+    
 }
