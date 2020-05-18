3e86e4315ebc60eafc6894ce0b6b68a4495da56c
Neil Mistry <P2432300@my365.dmu.ac.uk>
2020-05-18 06:19:17 +0100

    changes made to code

--- FootballMerch FrontEnd/AnStock.aspx.cs
+++ FootballMerch FrontEnd/AnStock.aspx.cs
@@ -7,6 +7,7 @@ using System.Web.UI.WebControls;
 using FootballMerch_Classes;
 
 
+
 public partial class AnStock : System.Web.UI.Page
 {
     //variable to store the primary key with page level scope
